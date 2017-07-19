295409b526ddcf5582a86d5b98ab94a2624ac444
Stephanie McIlroy <stephanie.mcilroy@gmail.com>
2017-07-18 22:30:51 -0400

    adding css

--- script.js
+++ script.js
@@ -5,27 +5,31 @@ app.controller('lab12Ctrl', function($scope) {
 
 
  $scope.gNames = ['Sophia', 'Emma', 'Olivia', 'Ava', 'Mia', 'Isabella', 'Riley', 'Aria', 'Zoe', 'Charlotte'];
-
  $scope.addGName =function(name) {
-
 $scope.gNames.push(name);
 
 
 };
 
 $scope.bNames = ['Jackson', 'Aiden', 'Lucas', 'Liam', 'Noah', 'Ethan','Mason', 'Caden', 'Oliver', 'Elijah'];
-
 $scope.addBName =function(name) {
-
 $scope.bNames.push(name);
 
 };
 
+
+$scope.removeGName = function (name) {
+var index = $scope.gNames.indexOf(name);
+$scope.gNames.splice(index, 1);
+
+}
+
+$scope.removeBName = function (name) {
+var index = $scope.bNames.indexOf(name);
+$scope.bNames.splice(index, 1);
+
+
+}
+
+
 });
-
-
-// $scope.names = [];
-// $scope.addName= function(usersName){
-//
-//
-//   $scope.names.push(usersName);
